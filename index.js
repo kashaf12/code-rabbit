620974ea9321c5b73e732d44085ccbe791cc3fbe
Kashaf Ahmed <kashafaahmed@gmail.com>
2024-04-15 14:19:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+class Nod {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkList {
+    constructor() {
+        this.head = null;
+    }
+
+    bfsTrav() {
+        let res = [];
+        let q = [];
+        
+        if (this.head === null) {
+            return res;
+        }
+        
+        q.push(this.head);
+        
+        while (q.length > 0) {
+            let cur = q.shift();
+            res.push(cur.val);
+            
+            if (cur.next !== null) {
+                q.push(cur.next);
+            }
+        }
+        
+        return res;
+    }
+}
